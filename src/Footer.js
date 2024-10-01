232aa6818e747f5a3a38bcc9a4fc57b49325c411
Matt <kean.wisc@gmail.com>
2024-10-01 15:39:31 -0400

    Adding some more framework to the boilerplate code.

---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+
+function Footer() {
+    return (
+        <footer>
+            <div class="doormat navigation">
+                <label>Doormat Navigation</label>
+                <ul>
+                    <li><a href="home">Home</a></li>
+                    <li><a href="about">About</a></li>
+                    <li><a href="menu">Menu</a></li>
+                    <li><a href="reservations">Reservations</a></li>
+                    <li><a href="order">Order Online</a></li>
+                    <li><a href="login">Login</a></li>
+                </ul>
+            </div>
+            <div class="contact">
+                <label>Contact Us</label>
+            </div>
+            <div class="social">
+                <label>Social Links</label>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+
+function Header() {
+    return (
+        <header>
+            <img src="./Logo192.png"></img>
+        </header>
+    );
+}
+
+export default Header;
