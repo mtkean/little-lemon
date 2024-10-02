115dcc9d8ea4b2f17f166037798cd4405c14fbd8
Matt <kean.wisc@gmail.com>
2024-10-02 17:31:20 -0400

    Adding some initial styling to the application

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
-import Nav from './Nav';
 
 function App() {
   return (
     <>
     <Header/>
-    <Nav/>
     <Main/>
     <Footer/>
     </>
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,10 @@
+import Nav from './Nav';
 
 function Header() {
     return (
         <header>
-            <img src="./Logo192.png"></img>
+            <img src="./Logo192.png" alt="A logo for the Little Lemon Restaurant"></img>
+            <Nav/>
         </header>
     );
 }
